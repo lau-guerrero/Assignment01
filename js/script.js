ca2828fa216021f251dd81b0ecf5474c99bffe37
lau-guerrero <laura.guerrero.gm@gmail.com>
2025-09-24 11:16:44 -0700

    step4

--- js/script.js
+++ js/script.js
@@ -14,11 +14,35 @@
 // STEP 3
 
 // Example 1 – Arithmetic with variables
-let x = 10;
-let y = 5;
-let result = (x * y) + 3;
+// let x = 10;
+// let y = 5;
+// let result = (x * y) + 3;
 
-//Example 2 – String concatenation with variables
-let firstName = "Laura";
-let lastName = "Guerrero";
-let fullName = firstName + " " + lastName;
+// //Example 2 – String concatenation with variables
+// let firstName = "Laura";
+// let lastName = "Guerrero";
+// let fullName = firstName + " " + lastName;
+
+// STEP 4
+
+//CamelCase
+let firstName;
+let lastName;
+let address;
+let city;
+let state;
+let zipCode;
+let yourAge;
+let referralSource;
+let mayWeContactYou;
+
+//Hungarian Notation
+let strFirstName;
+let strLastName;
+let strAddress;
+let strCity;
+let strState;
+let strZipCode;
+let numYourAge;
+let strReferralSource;
+let boolMayWeContactYou;
