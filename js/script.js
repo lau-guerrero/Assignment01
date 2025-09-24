2ce4c93df7170ef58863425644135cf93c856b9c
lau-guerrero <laura.guerrero.gm@gmail.com>
2025-09-24 10:41:34 -0700

    step2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 // STEP 1
-let someMonth
-function theMonth() {}
-let currentMonth
-let summerMonth
-let myLibraryFunction
+// let someMonth
+// function theMonth() {}
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
 
 // STEP 2
+let age = 25;                // numeric literal expression
+let greeting = "Hello!";     // string literal expression
+let isLoggedIn = false;      // boolean literal expression
+let selectedItem = null;     // null literal expression
 
 // STEP 3
